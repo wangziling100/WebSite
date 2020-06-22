bdb5eb2eda1f10ab5b999b32da72d429839d030c
Bob Wang <wangziling1000@gmail.com>
2020-06-22 19:20:35 +0200

    lambda, rewrite for batch commands

--- lambda/push/push/app.js
+++ lambda/push/push/app.js
@@ -5,7 +5,70 @@ const client = new SiteClient(token)
 let response
 let atob = require('atob')
 
+const processData = async (data) => {
+    if (data.option===undefined || data.option==='edit'){
+        let tmpRef = data.ref
+        if (data.password===password){
+            switch (data.ref){
+                case 'idea_new': tmpRef='idea_item'; break;
+                case 'plan_new': tmpRef='plan_item'; break;
+            }
+            if (data.option==='edit'){
+                let historyRef
+                switch (data.ref){
+                    case 'idea_new': historyRef='idea_history'; break;
+                }
+                await client.items.update(data.refId, {
+                    ref: historyRef,
+                }).catch((err)=>{
+                    console.error(err)
+                })
+            }
+
+        }
+        await client.items.create({
+            itemType: "238671",
+            title: data.title,
+            content: data.content,
+            priority: data.priority,
+            completeness: data.completeness,
+            startTime: data.startTime, 
+            evaluation:  data.evaluation,
+            allowPriorityChange: data.allowPriorityChange,
+            ref: tmpRef,
+            refId: data.refId,
+            owner: data.owner,
+            contributor: data.contributor,
+            tag: data.tag,
+            itemStatus: data.itemStatus,
+            version: data.version,
+            layer: data.layer,
+            parents: data.parents,
+        })
+    }else if(data.option==='delete' && data.password===password){
+        await client.items.destroy(data.id)
+            .catch((error) => {
+                console.error(error);
+            })
+
+    }else if(data.option==='completed' && data.password===password){
+        await client.items.update(data.id, {
+            itemStatus: "completed",
+        }).catch((err) => {
+            console.error(err)
+        })
+    }else if(data.option==='active' && data.password===password){
+        await client.items.update(data.id, {
+            itemStatus: "active",
+        }).catch((err) => {
+            console.error(err)
+        })
+    }else{
+        console.log('unknown option')
+    }
+}
 exports.lambdaHandler = async (event, context) =>{
+    
     try{
         let isBase64 = false
         try{
@@ -24,65 +87,16 @@ exports.lambdaHandler = async (event, context) =>{
         if(!isBase64){
             data = JSON.parse(event.body)
         }
-        console.log(typeof(data))
-        if (data.option===undefined || data.option==='edit'){
-            let tmpRef = data.ref
-            if (data.password===password){
-                switch (data.ref){
-                    case 'idea_new': tmpRef='idea_item'; break;
-                    case 'plan_new': tmpRef='plan_item'; break;
-                }
-                if (data.option==='edit'){
-                    let historyRef
-                    switch (data.ref){
-                        case 'idea_new': historyRef='idea_history'; break;
-                    }
-                    await client.items.update(data.refId, {
-                        ref: historyRef,
-                    }).catch((err)=>{
-                        console.error(err)
-                    })
-                }
 
+        if (data.data === undefined){
+            await processData(data)
+        }else{
+            // array
+            for (element of data.data){
+                await processData(element)
             }
-            await client.items.create({
-                itemType: "238671",
-                title: data.title,
-                content: data.content,
-                priority: data.priority,
-                completeness: data.completeness,
-                startTime: data.startTime, 
-                evaluation:  data.evaluation,
-                allowPriorityChange: data.allowPriorityChange,
-                ref: tmpRef,
-                refId: data.refId,
-                owner: data.owner,
-                contributor: data.contributor,
-                tag: data.tag,
-                itemStatus: data.itemStatus,
-                version: data.version,
-                layer: data.layer,
-                parents: data.parents,
-            })
-        }else if(data.option==='delete' && data.password===password){
-            await client.items.destroy(data.id)
-                .catch((error) => {
-                    console.error(error);
-                })
-
-        }else if(data.option==='completed' && data.password===password){
-            await client.items.update(data.id, {
-                itemStatus: "completed",
-            }).catch((err) => {
-                console.error(err)
-            })
-        }else if(data.option==='active' && data.password===password){
-            await client.items.update(data.id, {
-                itemStatus: "active",
-            }).catch((err) => {
-                console.error(err)
-            })
-        }        
+        }
+                
 	// const ret = await axios(url);
         response = {
             'statusCode': 200,
